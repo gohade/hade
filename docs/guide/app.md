bbcc3b326ad424d87dac6778f5169b799d585e5d
jianfengye <jianfengye110@gmail.com>
2023-01-02 19:41:51 +0800

    app config sequence update

--- docs/guide/app.md
+++ docs/guide/app.md
@@ -2,7 +2,8 @@
 
 ## 命令
 
-这里的运行是运行整个 app，这个 app 可以只包含后端，也可以只包含前端，但是后端也是隐藏在前端后面运行。具体可以参考 app/http/route.go
+这里的运行是运行整个 app，这个 app 可以只包含后端，也可以只包含前端，但是后端也是隐藏在前端后面运行。具体可以参考
+app/http/route.go
 
 ```
 package http
@@ -45,12 +46,27 @@ Use "hade app [command] --help" for more information about a command.
 
 可以使用 `./hade app start` 启动一个应用。
 
+```markdown
+成功启动进程: hade app
+进程pid: 39327
+监听地址: http://localhost:8888
+基础路径: /Users/jianfengye/Documents/workspace/gohade/hade/
+日志路径: /Users/jianfengye/Documents/workspace/gohade/hade/teststorage/log
+运行路径: /Users/jianfengye/Documents/workspace/gohade/hade/teststorage/runtime
+配置路径: /Users/jianfengye/Documents/workspace/gohade/hade/config
+```
+
 也可以使用 `./hade app start -d` 使用 deamon 模式启动一个应用。应用名称为 `hade app`
 
 ```
 [~/Documents/workspace/hade_workspace/demo5]$ ./hade app start -d
-app serve started
-log file: /Users/Documents/workspace/hade_workspace/demo5/storage/log/app.log
+成功启动进程: hade app
+进程pid: 41021
+监听地址: http://localhost:8888
+基础路径: /Users/jianfengye/Documents/workspace/gohade/hade/
+日志路径: /Users/jianfengye/Documents/workspace/gohade/hade/teststorage/log
+运行路径: /Users/jianfengye/Documents/workspace/gohade/hade/teststorage/runtime
+配置路径: /Users/jianfengye/Documents/workspace/gohade/hade/config
 ```
 
 app 应用的输出记录在 `/storage/log/app.log`
@@ -63,7 +79,7 @@ app 应用的输出记录在 `/storage/log/app.log`
 
 ```
 [~/Documents/workspace/hade_workspace/demo5]$ ./hade app state
-app server started, pid: 28170
+app服务已经启动, pid: 41021
 ```
 
 ## 重启
@@ -77,3 +93,65 @@ app server started, pid: 28170
 ## 停止
 
 当使用 deamon 模式启动的时候，需要关闭应用，可以使用命令 `./hade app stop`
+
+## 进程运行基础配置
+
+在启动进程的时候，我们会需要定义一些配置项，这些配置项决定进程的运行环境（比如日志存放位置，运行态信息存放位置，配置文件存放位置等）。
+
+这里我们提供了3种配置方式来设置这些基础配置，包括环境变量设置，命令行参数设置，配置文件设置。
+
+这三种配置方式的优先级为：命令行参数 > 环境变量 > 配置文件
+
+具体的配置项常用的如下，具体更多可以参考 framework/provider/app/service.go ：
+
+* 运行中间信息存放目录
+    * 命令行参数：--runtime_folder
+    * 环境变量：RUNTIME_FOLDER
+    * 配置文件：app.path.runtime_folder
+    * 不设置默认为：运行信息基础目录 + runtime
+* 日志存放目录
+    * 命令行参数：--log_folder
+    * 环境变量：LOG_FOLDER
+    * 配置文件：app.path.log_folder
+    * 不设置默认为：运行信息基础目录 + log
+* 运行信息基础目录
+    * 命令行参数：--storage_folder
+    * 环境变量：STORAGE_FOLDER
+    * 配置文件：app.path.storage_folder
+    * 不设置默认为：基础目录 + storage
+* 配置文件地址
+    * 命令行参数：--config_folder
+    * 环境变量：CONFIG_FOLDER
+    * 配置文件：app.path.config_folder
+    * 不设置默认为：基础目录 + config
+* 基础目录
+    * 命令行参数：--base_folder
+    * 环境变量：BASE_FOLDER
+    * 配置文件：app.path.base_folder
+    * 不设置默认为：当前执行目录
+
+### 环境变量设置
+
+在启动进程的时候进行环境变量的设置。比如
+
+```markdown
+STORAGE_FOLDER=/Users/jianfengye/Documents/workspace/gohade/hade/teststorage ./hade app start
+```
+
+### 命令行参数设置
+
+在命令行参数中设置。比如
+
+```markdown
+./hade app start --storage_folder=/Users/jianfengye/Documents/workspace/gohade/hade/teststorage/ -d
+```
+
+### 配置文件设置
+
+在配置文件config/${env}/app.yaml中配置：
+
+```markdown
+path:
+storage_folder: "/Users/jianfengye/Documents/workspace/gohade/hade/teststorage/"
+```
+
--- framework/provider/app/service.go
+++ framework/provider/app/service.go
@@ -1,12 +1,13 @@
 package app
 
 import (
-	"flag"
 	"github.com/gohade/hade/framework"
 	"github.com/gohade/hade/framework/util"
 	"github.com/google/uuid"
 	"github.com/pkg/errors"
+	"os"
 	"path/filepath"
+	"strings"
 )
 
 // HadeApp 代表hade框架的App实现
@@ -16,6 +17,8 @@ type HadeApp struct {
 	appId      string              // 表示当前这个app的唯一id, 可以用于分布式锁等
 
 	configMap map[string]string // 配置加载
+	envMap    map[string]string // 环境变量加载
+	argsMap   map[string]string // 参数加载
 }
 
 // AppID 表示这个App的唯一ID
@@ -33,6 +36,10 @@ func (app HadeApp) BaseFolder() string {
 	if app.baseFolder != "" {
 		return app.baseFolder
 	}
+	baseFolder := app.getConfigBySequence("base_folder", "BASE_FOLDER", "app.path.base_folder")
+	if baseFolder != "" {
+		return baseFolder
+	}
 
 	// 如果参数也没有，使用默认的当前路径
 	return util.GetExecDirectory()
@@ -40,7 +47,8 @@ func (app HadeApp) BaseFolder() string {
 
 // ConfigFolder  表示配置文件地址
 func (app HadeApp) ConfigFolder() string {
-	if val, ok := app.configMap["config_folder"]; ok {
+	val := app.getConfigBySequence("config_folder", "CONFIG_FOLDER", "app.path.config_folder")
+	if val != "" {
 		return val
 	}
 	return filepath.Join(app.BaseFolder(), "config")
@@ -48,36 +56,42 @@ func (app HadeApp) ConfigFolder() string {
 
 // LogFolder 表示日志存放地址
 func (app HadeApp) LogFolder() string {
-	if val, ok := app.configMap["log_folder"]; ok {
+	val := app.getConfigBySequence("log_folder", "LOG_FOLDER", "app.path.log_folder")
+	if val != "" {
 		return val
 	}
 	return filepath.Join(app.StorageFolder(), "log")
 }
 
 func (app HadeApp) HttpFolder() string {
-	if val, ok := app.configMap["http_folder"]; ok {
+	val := app.getConfigBySequence("http_folder", "HTTP_FOLDER", "app.path.http_folder")
+	if val != "" {
 		return val
 	}
 	return filepath.Join(app.BaseFolder(), "app", "http")
 }
 
 func (app HadeApp) ConsoleFolder() string {
-	if val, ok := app.configMap["console_folder"]; ok {
+	val := app.getConfigBySequence("console_folder", "CONSOLE_FOLDER", "app.path.console_folder")
+	if val != "" {
 		return val
 	}
 	return filepath.Join(app.BaseFolder(), "app", "console")
 }
 
 func (app HadeApp) StorageFolder() string {
-	if val, ok := app.configMap["storage_folder"]; ok {
+	val := app.getConfigBySequence("storage_folder", "STORAGE_FOLDER", "app.path.storage_folder")
+	if val != "" {
 		return val
 	}
+
 	return filepath.Join(app.BaseFolder(), "storage")
 }
 
 // ProviderFolder 定义业务自己的服务提供者地址
 func (app HadeApp) ProviderFolder() string {
-	if val, ok := app.configMap["provider_folder"]; ok {
+	val := app.getConfigBySequence("provider_folder", "PROVIDER_FOLDER", "app.path.provider_folder")
+	if val != "" {
 		return val
 	}
 	return filepath.Join(app.BaseFolder(), "app", "provider")
@@ -85,7 +99,8 @@ func (app HadeApp) ProviderFolder() string {
 
 // MiddlewareFolder 定义业务自己定义的中间件
 func (app HadeApp) MiddlewareFolder() string {
-	if val, ok := app.configMap["middleware_folder"]; ok {
+	val := app.getConfigBySequence("middleware_folder", "MIDDLEWARE_FOLDER", "app.path.middleware_folder")
+	if val != "" {
 		return val
 	}
 	return filepath.Join(app.HttpFolder(), "middleware")
@@ -93,7 +108,8 @@ func (app HadeApp) MiddlewareFolder() string {
 
 // CommandFolder 定义业务定义的命令
 func (app HadeApp) CommandFolder() string {
-	if val, ok := app.configMap["command_folder"]; ok {
+	val := app.getConfigBySequence("command_folder", "COMMAND_FOLDER", "app.path.command_folder")
+	if val != "" {
 		return val
 	}
 	return filepath.Join(app.ConsoleFolder(), "command")
@@ -101,7 +117,8 @@ func (app HadeApp) CommandFolder() string {
 
 // RuntimeFolder 定义业务的运行中间态信息
 func (app HadeApp) RuntimeFolder() string {
-	if val, ok := app.configMap["runtime_folder"]; ok {
+	val := app.getConfigBySequence("runtime_folder", "RUNTIME_FOLDER", "app.path.runtime_folder")
+	if val != "" {
 		return val
 	}
 	return filepath.Join(app.StorageFolder(), "runtime")
@@ -109,7 +126,8 @@ func (app HadeApp) RuntimeFolder() string {
 
 // TestFolder 定义测试需要的信息
 func (app HadeApp) TestFolder() string {
-	if val, ok := app.configMap["test_folder"]; ok {
+	val := app.getConfigBySequence("test_folder", "TEST_FOLDER", "app.path.test_folder")
+	if val != "" {
 		return val
 	}
 	return filepath.Join(app.BaseFolder(), "test")
@@ -117,10 +135,20 @@ func (app HadeApp) TestFolder() string {
 
 // DeployFolder 定义测试需要的信息
 func (app HadeApp) DeployFolder() string {
-	if val, ok := app.configMap["deploy_folder"]; ok {
+	val := app.getConfigBySequence("deploy_folder", "DEPLOY_FOLDER", "app.path.deploy_folder")
+	if val != "" {
 		return val
 	}
 	return filepath.Join(app.BaseFolder(), "deploy")
+}
+
+// AppFolder 代表app目录
+func (app *HadeApp) AppFolder() string {
+	val := app.getConfigBySequence("app_folder", "APP_FOLDER", "app.path.app_folder")
+	if val != "" {
+		return val
+	}
+	return filepath.Join(app.BaseFolder(), "app")
 }
 
 // NewHadeApp 初始化HadeApp
@@ -132,14 +160,64 @@ func NewHadeApp(params ...interface{}) (interface{}, error) {
 	// 有两个参数，一个是容器，一个是baseFolder
 	container := params[0].(framework.Container)
 	baseFolder := params[1].(string)
-	// 如果没有设置，则使用参数
-	if baseFolder == "" {
-		flag.StringVar(&baseFolder, "base_folder", "", "base_folder参数, 默认为当前路径")
-		flag.Parse()
-	}
+
 	appId := uuid.New().String()
 	configMap := map[string]string{}
-	return &HadeApp{baseFolder: baseFolder, container: container, appId: appId, configMap: configMap}, nil
+	hadeApp := &HadeApp{baseFolder: baseFolder, container: container, appId: appId, configMap: configMap}
+	_ = hadeApp.loadEnvMaps()
+	_ = hadeApp.loadArgsMaps()
+	return hadeApp, nil
+}
+
+// GetConfigByDefault 默认获取配置项的方法
+// 配置优先级：参数>环境变量>配置文件
+func (app *HadeApp) getConfigBySequence(argsKey string, envKey string, configKey string) string {
+	if app.argsMap != nil {
+		if val, ok := app.argsMap[argsKey]; ok {
+			return val
+		}
+	}
+
+	if app.envMap != nil {
+		if val, ok := app.envMap[envKey]; ok {
+			return val
+		}
+	}
+
+	if app.configMap != nil {
+		if val, ok := app.configMap[configKey]; ok {
+			return val
+		}
+	}
+	return ""
+}
+
+func (app *HadeApp) loadEnvMaps() error {
+	if app.envMap == nil {
+		app.envMap = map[string]string{}
+	}
+	// 读取环境变量
+	for _, env := range os.Environ() {
+		pair := strings.SplitN(env, "=", 2)
+		app.envMap[pair[0]] = pair[1]
+	}
+	return nil
+}
+
+func (app *HadeApp) loadArgsMaps() error {
+	if app.argsMap == nil {
+		app.argsMap = map[string]string{}
+	}
+	// load args, must be format : --key=value
+	for _, arg := range os.Args {
+		if strings.HasPrefix(arg, "--") {
+			pair := strings.SplitN(strings.TrimPrefix(arg, "--"), "=", 2)
+			if len(pair) == 2 {
+				app.argsMap[pair[0]] = pair[1]
+			}
+		}
+	}
+	return nil
 }
 
 // LoadAppConfig 加载配置map
@@ -147,12 +225,4 @@ func (app *HadeApp) LoadAppConfig(kv map[string]string) {
 	for key, val := range kv {
 		app.configMap[key] = val
 	}
-}
-
-// AppFolder 代表app目录
-func (app *HadeApp) AppFolder() string {
-	if val, ok := app.configMap["app_folder"]; ok {
-		return val
-	}
-	return filepath.Join(app.BaseFolder(), "app")
 }
