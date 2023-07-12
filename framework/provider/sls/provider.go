b9b122e5a231e00d168fc7f91fa7fcd3469252cf
samdlcong <samnew@126.com>
2023-07-12 10:38:36 +0800

    add sls service test

--- framework/provider/sls/provider.go
+++ framework/provider/sls/provider.go
@@ -1,8 +1,8 @@
 package sls
 
 import (
-    "github.com/gohade/hade/framework"
-    "github.com/gohade/hade/framework/contract"
+	"github.com/gohade/hade/framework"
+	"github.com/gohade/hade/framework/contract"
 )
 
 type HadeSLSProvider struct {
@@ -10,25 +10,25 @@ type HadeSLSProvider struct {
 
 // Register registe a new function for make a service instance
 func (provider *HadeSLSProvider) Register(c framework.Container) framework.NewInstance {
-    return NewHadeSLSService
+	return NewHadeSLSService
 }
 
 // Boot will called when the service instantiate
 func (provider *HadeSLSProvider) Boot(c framework.Container) error {
-    return nil
+	return nil
 }
 
 // IsDefer define whether the service instantiate when first make or register
 func (provider *HadeSLSProvider) IsDefer() bool {
-    return true
+	return true
 }
 
 // Params define the necessary params for NewInstance
 func (provider *HadeSLSProvider) Params(c framework.Container) []interface{} {
-    return []interface{}{c}
+	return []interface{}{c}
 }
 
 // Name define the name for this service
 func (provider *HadeSLSProvider) Name() string {
-    return contract.SLSKey
+	return contract.SLSKey
 }
