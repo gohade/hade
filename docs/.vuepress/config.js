706ad66739677ffc0be32df013e7c0a19becf5c7
jianfengye <jianfengye110@gmail.com>
2023-07-30 00:17:18 +0800

    update vue doc

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -47,6 +47,7 @@ export default {
                         "model",
                         "deploy",
                         "util",
+                        "grpc",
                         "todo",
                     ],
                 },
